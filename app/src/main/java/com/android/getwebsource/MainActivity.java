eb1f3bd5eb07e2766dc72a86f760ad425e0342e1
waflol <lenguyenhunganh2001@gmail.com>
2022-04-27 23:13:30 +0700

    a

--- app/src/main/java/com/android/getwebsource/MainActivity.java
+++ app/src/main/java/com/android/getwebsource/MainActivity.java
@@ -4,11 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    TextView tv;
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        sp = (Spinner) findViewById(R.id.planets_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sp.setAdapter(adapter);
+        sp.setOnItemClickListener(this);
+        tv.setText("Page source will appear here");
+    }
+    public String getWebsite(String website){
+        String resString ="";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(website);
+        try{
+            HttpResponse response;
+            response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"windows-1251"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine())!=null){
+                sb.append(line+"\n");
+                resString = sb.toString();
+                is.close();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
+        }
+        return resString;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
