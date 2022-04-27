fcf44dcb9a08a4d41f40ee6f0692fbdea8ee6418
waflol <lenguyenhunganh2001@gmail.com>
2022-04-28 00:37:43 +0700

    done!

--- app/src/main/java/com/android/getwebsource/MainActivity.java
+++ app/src/main/java/com/android/getwebsource/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +27,9 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView tv;
     Spinner sp;
+    EditText Getinput;
+    String pre = "http://";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +37,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         tv = (TextView) findViewById(R.id.textView);
         sp = (Spinner) findViewById(R.id.planets_spinner);
+        Getinput = (EditText) findViewById(R.id.EditText);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_items, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         sp.setAdapter(adapter);
-        sp.setOnItemClickListener(this);
+        sp.setOnItemSelectedListener(this);
         tv.setText("Page source will appear here");
     }
     public String getWebsite(String website){
@@ -61,19 +66,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
         catch (Exception e){
             e.printStackTrace();
-            Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
         }
         return resString;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(),text,Toast.LENGTH_SHORT).show();
+        String pre = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(),pre,Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+        String pre = "http://";
+    }
+
+    public void GetSource(View view) {
+        String url = pre+Getinput.getText().toString();
+        try{
+            tv.setText(getWebsite(url));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
